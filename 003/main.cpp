665b03f759c70c64a176b8c7fa23f5b7de5c7fc0
OpenShift guest <github@deepaksandhu.com>
2013-09-19 19:02:29 -0400

    solve

--- 003/main.cpp
+++ 003/main.cpp
@@ -2,8 +2,18 @@
 #include <iostream>
 
 int main(int argc, char **argv){
+    long long input = 600851475143;
+    long long factor = 2;
     
-    std::cout << "Hello World" << std::endl;
+    while(factor * factor < input){
+        while(input % factor == 0){
+            //seperate newly found factor from largest factor
+            input /= factor;
+        }
+        factor++;
+    }
+    
+    std::cout << input << std::endl;
     
     return EXIT_SUCCESS;
 }
