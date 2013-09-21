65eeb78bf11585da37846df77f45b4836d5c1ad2
OpenShift guest <github@deepaksandhu.com>
2013-09-21 04:26:39 -0400

    solve

--- 004/main.cpp
+++ 004/main.cpp
@@ -1,9 +1,35 @@
 #include <cstdlib>
 #include <iostream>
+#include <stack>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+
+std::string ReverseString(std::string input);
+std::stringstream converter;
 
 int main(int argc, char **argv){
-    
-    std::cout << "Hello World" << std::endl;
+    int largest = 0;
+    for(int i = 100; i <= 999; i++){
+        for(int j = 100; j <= 999; j++){
+            converter << i*j;
+            
+            if(strcmp(ReverseString(converter.str()).c_str(), converter.str().c_str()) == 0){
+                largest = std::max(largest, i*j);
+            }
+            converter.str(std::string());
+        }
+    }
+    std::cout << largest << std::endl;
     
     return EXIT_SUCCESS;
+}
+
+std::string ReverseString(std::string input){
+    std::string output = "";
+    for(int i = input.length() - 1; i >= 0; i--){
+        output += input[i];
+    }
+    return output;
 }
