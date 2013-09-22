449260830a639f55bbad75b7a5861b53e56cc042
OpenShift guest <github@deepaksandhu.com>
2013-09-21 20:22:22 -0400

    solve

--- 009/main.cpp
+++ 009/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char **argv){
                                          << b << " "
                                          << c << " "
                                          << endl; 
+                        cout << "product: " << a*b*c << endl;
                     } 
                 } 
             } 
