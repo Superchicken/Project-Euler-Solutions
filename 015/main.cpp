757be5b058138c87efd822179eefffb756aa7236
OpenShift guest <github@deepaksandhu.com>
2013-09-26 17:15:15 -0400

    initial commit

---
+++ 015/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv){
+    //this problem can be solved with combinatorics
+    // 40!
+    //-----
+    //20!20!
+    
+    std::cout << "Hello World" << std::endl;
+    
+    return EXIT_SUCCESS;
+}
